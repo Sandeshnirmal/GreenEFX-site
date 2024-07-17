8cecc7d8cf1f7c906568138b61c2c81afce1825a
Sandesh Nirmal <88815877+Sandeshnirmal@users.noreply.github.com>
2024-07-17 15:45:11 +0530

    add popup to service

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-
-
 // function responsive(e){
-  AOS.init({
-    disable: function() {
-      var maxWidth =1000;
-      return window.innerWidth < maxWidth;
-    }
-  });
-let image1 = document.querySelector('#anime');
-let image2 = document.querySelectorAll('#anime1');
+AOS.init({
+  disable: function () {
+    var maxWidth = 1000;
+    return window.innerWidth < maxWidth;
+  },
+});
+let image1 = document.querySelector("#anime");
+let image2 = document.querySelectorAll("#anime1");
 
-document.addEventListener('mousemove', (e) =>{
-
-  let x = (window.innerWidth / 2.5  - e.pageX) / 80;
+document.addEventListener("mousemove", (e) => {
+  let x = (window.innerWidth / 2.5 - e.pageX) / 80;
   let y = (window.innerHeight / 2.5 - e.pageY) / 50;
 
   image1.style.transform = `rotateX(${-y}deg) rotateY(${-x}deg)`;
-  
-  [].forEach.call(image2, image2 =>{
-      image2.style.transform = `translateY(${y}px) translateX(${x}px)`;
-  });
 
+  [].forEach.call(image2, (image2) => {
+    image2.style.transform = `translateY(${y}px) translateX(${x}px)`;
+  });
 });
 
 // }
@@ -30,38 +26,34 @@ document.addEventListener('mousemove', (e) =>{
 // responsive(screen);
 // screen.addEventListener('change', responsive);
 
+// to show and hide Dropdown
 
-// to show and hide Dropdown 
-
-let myDropdown = document.getElementById("myDropdown")
+let myDropdown = document.getElementById("myDropdown");
 let display = 0;
 
-function myFunction(){
-  if(display == 1){
+function myFunction() {
+  if (display == 1) {
     myDropdown.style.display = "none";
     display = 0;
     myDropdown1.style.display = "none";
     display1 = 0;
-  }
-  else{
+  } else {
     myDropdown.style.display = "block";
     display = 1;
   }
 }
 
-let myDropdown1 = document.getElementById("myDropdown1")
+let myDropdown1 = document.getElementById("myDropdown1");
 let display1 = 0;
 
-function myFunction1(){
-  if(display1 == 1){
+function myFunction1() {
+  if (display1 == 1) {
     myDropdown1.style.display = "none";
     display1 = 0;
-  }
-  else{
+  } else {
     myDropdown1.style.display = "block";
     display1 = 1;
   }
- 
 }
 
 // to show nav bar in mobile
@@ -71,27 +63,27 @@ let showing = 0;
 
 // let width = window.matchMedia.apply("(max-width: 1000px)");
 
-function toShow(){
-    if(showing == 1){
-        content.style.display = "none";
-        showing = 0; 
-    }
-    else{
-        content.style.display = "block"; 
-        showing = 1;
-    }
-
-
+function toShow() {
+  if (showing == 1) {
+    content.style.display = "none";
+    showing = 0;
+  } else {
+    content.style.display = "block";
+    showing = 1;
+  }
 }
 
+// popup section
 
+let pop = document.getElementById("popup1");
+let displaying = 0;
 
-
-
-
-
-
-
-
-
-
+function popup() {
+  if (displaying == 1) {
+    pop.style.display = "none";
+    displaying = 0;
+  } else {
+    pop.style.display = "block";
+    displaying = 1;
+  }
+}
